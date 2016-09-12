16c35c28011fcf6338ae2eff6a483751b5d1df0e
mathieu.baudonnat <m.baudonnat@yahoo.fr>
2016-09-12 15:45:43 +0200

    version  12/09/16 responsive corrigé

--- wp-content/plugins/event-rocket/templates/embedded-events.php
+++ wp-content/plugins/event-rocket/templates/embedded-events.php
@@ -1,9 +1,10 @@
 <?php defined( 'ABSPATH' ) or exit() ?>
 
 <div class="eventrocket embedded-event post">
-	<div class="post-thumb"> <?php echo tribe_event_featured_image() ?> </div>
 	<h3> <a href="<?php the_permalink() ?>"><?php the_title() ?></a> </h3>
 	<div class="schedule"> <?php echo tribe_events_event_schedule_details() ?> </div>
-	<div class="schedule"> <?php echo tribe_get_venue() ?> </div>
+	<div class="post-thumb"> <?php echo tribe_event_featured_image() ?> </div>
+	<div class="schedule"> Place of the event : <?php echo tribe_get_venue() ?> </div>
 	<div class="summary"> <?php the_excerpt() ?> </div>
+	<div class="clearfix"></div>
 </div>
